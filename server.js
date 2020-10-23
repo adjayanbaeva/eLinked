276e4be839c88f01320009e322db83e15f04eae6
adjayanbaeva <adjayanbaeva@gmail.com>
2020-10-22 17:15:17 -0700

    define first route and test it

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const app = express();
+
+//First route
+app.get('/', (req, res) => res.send('Hello!'));
+
+const port = 8000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
