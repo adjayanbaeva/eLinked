702e8fb3e36411d5f9bd629371e8ac497c424570
adjayanbaeva <adjayanbaeva@gmail.com>
2020-10-24 20:48:36 -0700

    create users, posts and profile routes and test them out

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
 const app = express();
 
 //Db config
@@ -14,6 +18,11 @@ mongoose
 
 //First route
 app.get('/', (req, res) => res.send('Hello!'));
+
+//Use all routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
 
 const port = 8000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
