0037179f082723a05ac83b0887401e5e19725dbb
adjayanbaeva <adjayanbaeva@gmail.com>
2020-10-24 18:57:49 -0700

    bring in mongoose db in server.js, and connect to mongo database

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+//Db config
+const db = require("./config/keys").mongoURI;
+
+//Coneect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log('Mongodb connected'))
+  .catch((err) => console.log(err));
+
 
 //First route
 app.get('/', (req, res) => res.send('Hello!'));
