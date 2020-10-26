c79e0e54efb13131f47170d6ce42050e1a58b303
adjayanbaeva <adjayanbaeva@gmail.com>
2020-10-25 20:38:05 -0700

    bring User model into user.js file, delete test route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
 
-router.get('/test', (req, res) => res.json({msg: 'Users works!'}));
+// @route POST api/users/register
+// @descr Register user
+// @access Public
+
 
 module.exports = router;
