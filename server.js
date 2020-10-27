480822b216a8113012e4febe741741c7136ae44a
adjayanbaeva <adjayanbaeva@gmail.com>
2020-10-26 20:25:21 -0700

    bring body-parser in and configure it in server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
-
 const app = express();
+
+//Body parser configuration
+app.use(body-parser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 
 //Db config
 const db = require("./config/keys").mongoURI;
