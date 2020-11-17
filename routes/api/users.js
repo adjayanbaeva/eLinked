d652b68ecd5588a1382009478f154345a13c9078
adjayanbaeva <adjayanbaeva@gmail.com>
2020-11-16 22:16:57 -0800

    set expiration time to 30 min by adding third optional parameter to sign() method

--- routes/api/users.js
+++ routes/api/users.js
@@ -67,7 +67,7 @@ router.post('/login', (req, res) => {
                         //Payload
                         const payload = {id: user.id, name: user.name, avatar: user.avatar};
                         //sign token
-                        jwt.sign(payload, keys.secretOrPrivateKey)
+                        jwt.sign(payload, keys.secretOrPrivateKey, {expiresIn: 3600})
                     } else {
                         return res.status(400).json({password: 'Password incorrect'});
                     }
