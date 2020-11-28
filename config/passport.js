74f1fd00167546259793c36f2e413d2299ce63bd
adjayanbaeva <adjayanbaeva@gmail.com>
2020-11-27 23:37:44 -0800

    write passport arrow function in passport.js that will use new JwtStrategy and console.log the jwt_payload

--- config/passport.js
+++ config/passport.js
@@ -7,3 +7,9 @@ const keys = require('../config/keys');
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrPrivateKey;
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) =>{
+        condole.log(jwt_payload);
+    }))
+}
