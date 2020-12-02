42b7102603556110e8f3ded157ceeea9b8877a7a
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-01 21:37:31 -0800

    in profile.js delete test code and bring required libraries and files in

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/test', (req, res) => res.json({msg: 'Profile works!'}));
-
-module.exports = router;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
