60cbaefca0081361cb9ef3e624d5f8b726d806b8
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-06 13:32:32 -0800

    fix errors in profile validation file

--- validation/profile.js
+++ validation/profile.js
@@ -20,37 +20,37 @@ module.exports = function validateProfileInput(data){
         errors.skills = 'Skills field is required';
     }
 
-    if (isEmpty(data.website)){
+    if (!isEmpty(data.website)){
         if (!validator.isURL(data.website)){
             errors.website = 'Not a valid URL';
         }
     }
 
-    if (isEmpty(data.youtube)){
+    if (!isEmpty(data.youtube)){
         if(!validator.isURL(data.youtube)){
             errors.youtube = 'Not a valid URL';
         }
     }
 
-    if (isEmpty(data.twitter)){
+    if (!isEmpty(data.twitter)){
         if(!validator.isURL(data.twitter)){
             errors.twitter = 'Not a valid URL';
         }
     }
 
-    if (isEmpty(data.facebook)){
+    if (!isEmpty(data.facebook)){
         if (!validator.isURL(data.facebook)){
             errors.facebook = 'Not a valid URL';
         }
     }
 
-    if (isEmpty(data.linkedin)){
+    if (!isEmpty(data.linkedin)){
         if (!validator.isURL(data.linkedin)){
             errors.linkedin = 'Not a valid URL';
         }
     }
 
-    if (isEmpty(data.instagram)){
+    if (!isEmpty(data.instagram)){
         if (!validator.isURL(data.instagram)){
             errors.instagram = 'Not a valid URL';
         }
