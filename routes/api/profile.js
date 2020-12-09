ff6b89aacb8dd148f0d91906323b4adf5daef2f6
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-08 17:34:52 -0800

    write api to delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -278,8 +278,22 @@ router.delete(
             })
             .catch((err) => res.status(404).json(err));
     }
-
 )
 
+//@route DELETE api/profile
+//@desc Delete user and profile
+//@access private
+router.delete(
+    '/',
+    passport.authenticate('jwt', {session:false}),
+    (req,res) => {
+        Profile.findOneAndDelete({user: req.user.id})
+            .then(() => {
+                User.findOneAndDelete({_id: req.user.id})
+                    .then(() =>
+                    res.json({success: true}))
+            })
+    }
+)
 
 module.exports = router;
