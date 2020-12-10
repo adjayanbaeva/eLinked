53dfdd0906a5326615e1d89ecb4ef738d9a35cca
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-10 14:43:46 -0800

    add Navbar selector to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import Navbar from './components/Layout/Navbar';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>My React App</h1>
       </div>
     )
