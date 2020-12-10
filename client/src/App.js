f3b41929ed2e2f60fa2a457d081884a6295e1a58
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-10 14:52:05 -0800

    add Footer selector to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <h1>My React App</h1>
+        <Footer />
       </div>
     )
   }
