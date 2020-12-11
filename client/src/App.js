90ce1082fcd8106622b48050997f455b46ba6a09
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-11 15:46:06 -0800

    create Register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Layout/Navbar';
-import Footer from './components/Layout/Footer';
-import Landing from './components/Layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 
 class App extends Component {
