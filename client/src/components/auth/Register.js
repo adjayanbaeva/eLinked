84b3a7e4d392ffc7f6ddc73f075ac08cb1d9af07
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-13 23:31:31 -0800

    add a constructor in Register class and set the state of the component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+    constructor() {
+        super();
+        this.state ={
+            name:"",
+            email:"",
+            password:"",
+            password2:"",
+            errors:{}
+        }
+    }
     render(){
         return (
             <div className="register">
