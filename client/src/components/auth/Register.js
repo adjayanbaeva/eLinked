203914aead4dec22cb75f104d31f3e662fd7c644
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-15 17:54:10 -0800

    install bootstraps classnames, bring it to register component, bind className to classnames and add conditional statement stating when invalid styling should be applied to textbox

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
     constructor() {
@@ -29,13 +30,16 @@ class Register extends Component {
             password: this.state.password,
             password2: this.state.password2
         }
-        axios.post('/api/user/register', newUser)
+        axios.post('/api/users/register', newUser)
             .then(res => console.log(res))
-            .catch(err => console.log(err.response.data))
+            .catch(err => this.setState({errors: err.response.data}))
     }
 
 
     render(){
+        //shortcut
+        const {errors} = this.state; //deconstructed from const errors = this.state.errors;
+
         return (
             <div className="register">
                 <div className="container">
@@ -45,17 +49,17 @@ class Register extends Component {
                     <p className="lead text-center">Create your eLinked account</p>
                     <form onSubmit={this.onSubmit}>
                         <div className="form-group">
-                        <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} required />
+                        <input type="text" className={classnames("form-control form-control-lg", {"is-invalid": this.errors.name})} placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} required />
                         </div>
                         <div className="form-group">
-                        <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value = {this.state.email} onChange={this.onChange} />
+                        <input type="email" className={classnames("form-control form-control-lg", {"is-invalid": this.errors.email})} placeholder="Email Address" name="email" value = {this.state.email} onChange={this.onChange} />
                         <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                         </div>
                         <div className="form-group">
-                        <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value = {this.state.password} onChange={this.onChange} />
+                        <input type="password" className={classnames("form-control form-control-lg", {"is-invalid": this.errors.password})} placeholder="Password" name="password" value = {this.state.password} onChange={this.onChange} />
                         </div>
                         <div className="form-group">
-                        <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange} />
+                        <input type="password" className={classnames("form-control form-control-lg", {"is-invalid": this.errors.password2} )} placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange} />
                         </div>
                         <input type="submit" className="btn btn-info btn-block mt-4" />
                     </form>
