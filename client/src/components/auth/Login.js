4bb67e32ee9f93d7354bc9f81f98ab6c9f13e784
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-15 20:09:40 -0800

    write onSubmit function and bind form control to it

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+
 class Login extends Component {
     constructor(){
         super();
@@ -9,10 +10,18 @@ class Login extends Component {
             errors: {}
         }
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e){
         this.setState({[e.target.name] : e.target.value})
+    }
+    onSubmit(e){
+        e.preventDefault();
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        };
     }
     render(){
         return (
@@ -22,7 +31,7 @@ class Login extends Component {
                     <div className="col-md-8 m-auto">
                     <h1 className="display-4 text-center">Log In</h1>
                     <p className="lead text-center">Sign in to your eLinked account</p>
-                    <form action="dashboard.html">
+                    <form onSubmit={this.onSubmit}>
                         <div className="form-group">
                         <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value = {this.state.email} onChange = {this.onChange}/>
                         </div>
