94cd59fa1d38ecb91ae2e6f481defccc902d0210
adjayanbaeva <adjayanbaeva@gmail.com>
2020-12-17 19:45:12 -0800

    import Provider from react-redux and bring in store file, connect App component with store

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -11,16 +13,17 @@ import Login from './components/auth/Login';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path = "/" component = {Landing} />
-          <Route exact path = "/register" component = {Register} />
-          <Route exact path = "/login" component = {Login} />
-          <Footer />
-        </div>
-      </Router>
-      
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path = "/" component = {Landing} />
+            <Route exact path = "/register" component = {Register} />
+            <Route exact path = "/login" component = {Login} />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     )
   }
   
