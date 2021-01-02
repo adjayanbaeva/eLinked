01e20265a1be4d3054c6813d5a25e2157273d0c7
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-01 22:00:13 -0800

    within setAuthToken fuction add logic to delete token from header of the request, export the setAuthToken function

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -3,5 +3,9 @@ import axios from 'axios';
 const setAuthToken = token => {
     if (token){
         axios.defaults.headers.common['Authorization'] = token;
+    }else{
+        delete axios.defaults.headers.common['Authorization'];
     }
 }
+
+export default setAuthToken;
