a355fdeafc99815ed96ab257adf4c583fe07b298
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-01 22:16:24 -0800

    dispatch SET_CURRENT_USER call and pass decoded token as its payload

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from 'jwt-decode';
 
@@ -21,6 +21,12 @@ export const loginUser = (userData) => dispatch => {
 
             //Decode the token
             const decoded = jwt_decode(token);
+
+            //Dispatch SET_CURRENT_USER call
+            dispatch({
+                type: SET_CURRENT_USER,
+                payload: decoded
+            })
         }))
         .catch(err => dispatch({type: GET_ERRORS, payload: err.response.data}))
 }
