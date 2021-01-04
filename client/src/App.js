7222dd1fb68c5c081fbc6b18516ad86dfe3e615e
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-03 23:13:18 -0800

    in App.js write logic to handle situation when user steps away from the domain and comes back: check if token in local storage if it is then set token to auth header, decode the token and dispatch the SET_CURRENT_USER call for authReducer to pick up and write to the redux store

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,25 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import setAuthToken from './utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import { SET_CURRENT_USER } from './actions/types';
+
+//Check for token in case user steps away from domain for short period of time and comes back
+if(localStorage.jwtToken){
+
+  //Set token to Auth Header
+  setAuthToken(localStorage.jwtToken);
+
+  //Decode token
+  const decoded = jwt_decode(localStorage.jwtToken);
+
+  //Write data to Redux store
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded
+  })
+}
 
 
 class App extends Component {
