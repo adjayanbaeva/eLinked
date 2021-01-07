8eba88681e37d1e84096673b3ce625e5dc8ddfe3
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-06 21:23:58 -0800

    add HTML content to dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,2 +1,20 @@
 import React, {Component} from 'react';
 
+class Dashboard extends Component{
+    render(){
+        return(
+          <div className="dashboard">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <h1 className="display-4">Dashboard</h1>
+                 </div>
+               </div>
+            </div>
+          </div>
+
+        )
+    }
+}
+
+export default Dashboard
