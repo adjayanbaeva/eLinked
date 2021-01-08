0863fe07873258c200f605c8def7c4bf915775bd
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-07 20:21:49 -0800

    import Link library and add Create Profile button in Dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Dashboard extends Component{
     render(){
@@ -8,6 +9,10 @@ class Dashboard extends Component{
               <div className="row">
                 <div className="col-md-12">
                   <h1 className="display-4">Dashboard</h1>
+                  <p>You have not yet setup a profile, please add some info</p>
+                  <Link to="/create-profile" className="btn btn-lg btn-info">
+                   Create Profile
+                  </Link>
                  </div>
                </div>
             </div>
