a9717653541a1c4f6a10b907d730035017eb10d6
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-10 22:39:16 -0800

    add HTML content to CreateProfile component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -13,6 +13,65 @@ class CreateProfile extends Component{
                             Let's get some information to make your profile stand out
                         </p>
                         <small className="d-block pb-3">* = required fields</small>
+                        <form onSubmit={this.onSubmit}>
+                            <TextFieldGroup
+                            placeholder="* Profile Handle"
+                            name="handle"
+                            info="A unique handle for your profile URL. Your full name, company name, nickname"
+                            />
+                            <SelectListGroup
+                            placeholder="Status"
+                            name="status"
+                            info="Give us an idea of where you are at in your career"
+                            />
+                            <TextFieldGroup
+                            placeholder="Company"
+                            name="company"
+                            info="Could be your own company or one you work for"
+                            />
+                            <TextFieldGroup
+                            placeholder="Website"
+                            name="website"
+                            info="Could be your own website or a company one"
+                            />
+                            <TextFieldGroup
+                            placeholder="Location"
+                            name="location"
+                            info="City or city & state suggested (eg. Boston, MA)"
+                            />
+                            <TextFieldGroup
+                            placeholder="* Skills"
+                            name="skills"
+                            info="Please use comma separated values (eg.
+                                HTML,CSS,JavaScript,PHP"
+                            />
+                            <TextFieldGroup
+                            placeholder="Github Username"
+                            name="githubusername"
+                            info="If you want your latest repos and a Github link, include your username"
+                            />
+                            <TextAreaFieldGroup
+                            placeholder="Short Bio"
+                            name="bio"
+                            info="Tell us a little about yourself"
+                            />
+
+                            <div className="mb-3">
+                            <button
+                                type="button"
+                                className="btn btn-light"
+                            >
+                                Add Social Network Links
+                            </button>
+                            <span className="text-muted">Optional</span>
+                            </div>
+                        
+                            <input
+                            type="submit"
+                            value="Submit"
+                            className="btn btn-info btn-block mt-4"
+                            />
+              </form>
 
                     </div>
 
