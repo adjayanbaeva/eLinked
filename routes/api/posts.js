bfc13c76bac6fa67a30a0e7ce0f4fbf594b7eb45
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-13 23:22:15 -0800

    write Post api to post post to database

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+const passport = require('passport');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
-router.get('/test', (req, res) => res.json({msg: 'Posts works!'}));
+router.post(
+    '/',
+    passport.authenticate('jwt', { session: false}),
+    (req, res) => {
+        const newPost = new Post({
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar,
+            user: req.user.id
+        });
+
+        newPost.save().then((post) => res.json(post))
+    }
+)
 
 module.exports = router;
