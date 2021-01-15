09e770be735f9024080e18566a4842cc09a294cf
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-14 22:32:03 -0800

    call validatePostInput() function from create post api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const passport = require('passport');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
+const validatePostInput = require('../../validation/post');
 
 // @route   GET api/posts
 // @desc    Get posts
@@ -22,6 +23,11 @@ router.post(
     '/',
     passport.authenticate('jwt', { session: false}),
     (req, res) => {
+        const {errors, isValid} = validatePostInput(req.body);
+        if (!isValid){
+            return res.status(400).json(errors);
+        }
+
         const newPost = new Post({
             text: req.body.text,
             name: req.body.name,
