c072f082eb740bb5148a97645326c3b1ce19370b
adjayanbaeva <adjayanbaeva@gmail.com>
2021-01-16 23:10:59 -0800

    add logic within like a post api that handles the case when user likes the post that has already been liked by the user

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -71,4 +71,22 @@ router.delete(
     }
     )
 
+    // @route POST api/posts/like/:id
+    // @desc Like a post
+    // @access Private
+    router.post(
+        '/like/:id',
+        passport.authenticate('jwt', {session: false}),
+        (req, res) => {
+            Profile.findOne({user: req.user.id}).then((profile) => {
+                Post.findById(req.params.id)
+                    .then((post) => {
+                        if (post.likes.filter((like) => like.user.toString()===req.user.id).length>0){
+                            return res.status(400).json({alreadyliked: 'User already liked this post'})
+                        }
+                    })
+            })
+        }
+    )
+
 module.exports = router;
